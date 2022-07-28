4d22e7cd3e7b19e03ad684b026454593e60f9e14
DanikTre <73853216+DanikTre@users.noreply.github.com>
2022-07-28 10:02:03 +0300

    fix: base routes

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -8,12 +8,12 @@ export const VIEW = Object.freeze({
 });
 
 export const PATHS = Object.freeze({
-  [VIEW.MAIN]: "/main",
-  [VIEW.CARD]: "/card/:id",
-  [VIEW.ART]: "/art/:id",
-  [VIEW.TEST]: "/test",
-  [VIEW.LOGIN]: "/login",
-  [VIEW.REDIRECT]: "/",
+  [VIEW.MAIN]: "/newspaper/main",
+  [VIEW.CARD]: "/newspaper/card/:id",
+  [VIEW.ART]: "/newspaper/art/:id",
+  [VIEW.TEST]: "/newspaper/test",
+  [VIEW.LOGIN]: "/newspaper/login",
+  [VIEW.REDIRECT]: "/newspaper/",
 });
 
 export const getPath = (view) => PATHS[view];
